efbb1a8e365f39923fb8355134eac9ce5d9c0751
K-Tran2001 <khoatran135.246@gmail.com>
2024-03-13 10:21:04 +0700

    commit 24

--- app/folders2/modals/confirmModal.jsx
+++ app/folders2/modals/confirmModal.jsx
@@ -51,7 +51,7 @@ export default function ConfirmModal({
               <div className="flex justify-end space-x-2 mt-4">
                 <button
                   type="button"
-                  className="w-[20%] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                  className="w-[20%] text-white bg-secondary  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                   onClick={(e) => {
                     setVisible(false);
                   }}
@@ -60,7 +60,7 @@ export default function ConfirmModal({
                 </button>
                 <button
                   type="button"
-                  className="w-[20%] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                  className="w-[20%] text-white bg-primary  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                   onClick={() => {
                     callback();
                     setVisible(false);
